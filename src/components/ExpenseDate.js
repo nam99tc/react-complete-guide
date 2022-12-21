29c4e71961b2df9fa7943e6a03fde68be41b8aa8
luong dinh nam <nam99tc@gmail.com>
2022-12-21 17:04:33 +0700

    filter

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,7 +1,7 @@
 import './ExpenseDate.css'
 const ExpenseDate = (props) => {
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('vi-VN', {day: '2-digit'});
+    const month = props.date.toLocaleString('vi-VN', {month: 'long'});
     const year = props.date.getFullYear();
   return (
     <div className="date">
